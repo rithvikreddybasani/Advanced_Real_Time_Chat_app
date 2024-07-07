d0071d1eaa2e5364d4d2f68b263e8406c5ceb031
rithvik reddy <147538307+rithvikreddybasani@users.noreply.github.com>
2024-07-07 22:14:58 +0530

    Update uploadMiddleware.js

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -1,7 +1,7 @@
 import { HttpError } from '../helpers/index.js';
 
 export const uploadMiddleware = (req, res, next) => {
-  // Check if the file has arrived and if its size exceeds 5MB
+  // Check if the file has arrived and if its size exceeds 5MB else it would give error in the firebase
   if (req.files && req.files.img.size > 5 * 1024 * 1024) {
     throw HttpError(400, 'File size exceeds 5 MB');
   }
