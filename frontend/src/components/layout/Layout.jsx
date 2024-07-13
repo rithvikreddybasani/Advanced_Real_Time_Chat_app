452a5a1906368cd6887217de0dc42cdfe9a984ba
rithvik reddy <147538307+rithvikreddybasani@users.noreply.github.com>
2024-07-13 14:10:15 +0530

    Update Layout.jsx

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Settings from '../features/navbar/dropdown.content/Settings';
-
+// from settings
 const Layout = () => {
   return (
     <main className='flex flex-col items-center justify-center h-[100dvh] max-w-full overflow-hidden text-slate-800'>
